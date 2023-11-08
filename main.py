907ebcbacd9ffd43ac70e4c6fd8310683eabf0d2
jakehulberg <149437250+jakehulberg@users.noreply.github.com>
2023-11-08 09:30:46 -0600

    Initial imports
    Using pip3 to download some modules. Going to try out Pandas later this week.

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import pandas
+
+
+
+# I will get to work on this soon. I wanted to try pip install pandas in the terminal
+# here and just see if it works.
+
+# That was easy. pip3 is the command. Not pip. Pandas installed!
