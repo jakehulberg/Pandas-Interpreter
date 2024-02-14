0b2e820c94646797c93be11a1a6c134b05ec8a09
jakehulberg <149437250+jakehulberg@users.noreply.github.com>
2024-02-13 19:43:17 -0600

    Python version change

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
-import pandas
-
 
 
 # I will get to work on this soon. I wanted to try pip install pandas in the terminal
 # here and just see if it works.
-
 # That was easy. pip3 is the command. Not pip. Pandas installed!
+
+with open("/Users/jakehulberg/Documents/Idiomas videos to do.pdf", "r") as file2:
+    file_readable = file2.read()
+
+print(file_readable)
+
+
